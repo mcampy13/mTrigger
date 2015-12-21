31d9f487229eba9c4f640700dac35a6bc89583cb
mcampy13 <mcampy13@gmail.com>
2015-12-21 13:34:34 -0500

    added auto email on form submit

--- emailForm.php
+++ emailForm.php
@@ -33,6 +33,90 @@
                     $statement=$dataBase->prepare("INSERT INTO emailForm (fName, lName, Email, City, State, Comments, mTriggerInterest) VALUES ('$fName', '$lName','$Email','$City','$State','$Comments','$mTriggerInterest')");
                     $statement->execute();
                     
+                    //Send email to info@mymtrigger.com after form is submitted
+                      
+                    $to= "admin@mymtrigger.com";
+              
+                  // Your subject
+              
+                    $subject="New Comment/Mailing List Sign-up From mymtrigger.com";
+              
+                  // From
+              
+                    $header="from: mTrigger_auto_no-reply";
+              
+                  // Your message
+              
+                    $message=" \r\n";
+              
+                    $message.="This user just just filled out the form with the following information: \r\n";
+              
+                    $message.="name:  $fName $lName \r\n";
+                    $message.="email: $Email \r\n";
+                    
+                    if ($City) {
+                        $message.= "City: $City \r\n";
+                    }
+                    
+                    if ($State){
+                        $message.="State: $State \r\n";
+                    }
+                    
+                    if($Comments){
+                        $message.="Comment: $Comments \r\n";
+                    }
+                    
+                    if($mTriggerInterest == 1){
+                        $message.="Add To Mailing List?: Yes \r\n";
+                        
+                        //Send message to new sign-up
+                        
+                            $to2= "$Email";
+                  
+                            // Your subject
+                    
+                            $subject2="Thanks For Signing Up To The mTrigger Mailing List";
+                      
+                          // From
+                      
+                            $header2="from: mTrigger_auto_no-reply ";
+                      
+                          // Your message
+                      
+                            $message2=" \r\n";
+                      
+                            $message2.="Thanks for signing up $fName. You will now receive emails about mTrigger updates and innovations. \r\n";
+                      
+                            $message2.="To be removed from our mailing list, kindly send an email to info@mymtrigger.com \r\n";
+                            
+                            $sentmail = mail($to2,$subject2,$message2,$header2);
+                      }
+                    
+                    else if($mTriggerInterest == 2){
+                        $message.="Add To Mailing List?: No \r\n";
+                    }
+    
+                    // send email
+              
+                    $sentmail = mail($to,$subject,$message,$header);
+            
+              
+                    // if your email succesfully sent
+              
+                    if($sentmail){
+              
+                        echo "Email sent";
+              
+                    }
+              
+                    else {
+              
+                    echo "Cannot send Confirmation link to your e-mail address";
+
+                     }
+                    
+                    
+                    
                 }
                 catch(PDOException $e) {
                       echo "Error Inserting into database";
